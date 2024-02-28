3611d900753ff6d1ce37e5b13b146a55f2f0e9cb
Andres Argote <andresargote2019@gmail.com>
2024-02-28 21:51:55 +0100

    :sparkles: tailwind set up

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,30 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['var(--font-inter)', 'system-ui'],
+        serif: ['var(--font-lexend)', 'system-ui'],
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        teal: '#357A7B',
+        lightCyan: '#D6F2F2',
+        charcoal: '#18181B',
+        black: '#000000',
+        white: '#FFFFFF',
+        steelBlue: '#94A3B8',
+        lightGray: '#D9D9D9',
       },
     },
   },
